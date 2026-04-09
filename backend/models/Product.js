a9f3b7d2c7c181c27a0d642292b844bd235a3508
Alex Agboli <alexcasimia@gmail.com>
2026-04-09 13:36:55 +0100

    Create Product.js

---
+++ backend/models/Product.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  brand: {
+    type: String,
+    required: true
+  },
+  category: {
+    type: String,
+    enum: ['phones', 'accessories'],
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  images: [String],
+  stock: {
+    type: Number,
+    required: true,
+    default: 0
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Product', productSchema);
