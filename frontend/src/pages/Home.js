01b5cedaf3d9e94ee76c4744f04117933927c826
Alex Agboli <alexcasimia@gmail.com>
2026-04-09 18:25:00 +0100

    Update Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Banner from '../components/Banner';
 import api from '../utils/api';
-import './Home.css';
 
 const Home = () => {
   const [banners, setBanners] = useState([]);
